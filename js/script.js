99e2d21f722567209529a6bac70e3eed37caa237
nafisahnaila241-crypto <nafisahnaila241@smk.belajar.id>
2026-09-14 00:15:42 +0700

    feat: tambah jam dan lokasi fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
-    console.log("Promo Kopi Nusa berhasil ditampilkan.");
+    promoButton.textContent = "Promo: Beli 3 Gratis 1!";
+    console.log("Promo Kopakopi berhasil ditampilkan.");
 });
